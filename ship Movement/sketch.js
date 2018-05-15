be69c3f8c9d29a826bc91912975d1f61414721ba
vanvolt <36053988+vanvolt@users.noreply.github.com>
2018-05-15 14:34:28 -0600

    Movement Fixes

--- ship Movement/sketch.js
+++ ship Movement/sketch.js
@@ -1,5 +1,6 @@
 let ship, shlep, shlop;
 let x, y;
+let isMovingUp, isMovingDown, isMovingRight, isMovingLeft;
 
 function preload() {
   ship = loadImage("assets/Ship.png");
@@ -11,27 +12,61 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   x = width/2;
   y = 600;
+  isMovingUp = false;
+  isMovingDown = false;
+  isMovingLeft = false;
+  isMovingRight = false;
 }
 
 function draw() {
   background(0);
-  if (keyIsPressed) { 
-    keyPressed();
+  if (keyIsPressed) {
+    moveShip();
   }
   image(ship,x,y);
 }
 
 function keyPressed() {
-  if (key === "d" || key === "D") {
-    x += 10;
+  if (key === "w" || key === "W") {
+    isMovingUp = true;
+  }
+  else if (key === "s" || key === "S") {
+    isMovingDown = true;
   }
   if (key === "a" || key === "A") {
-    x -= 10;
+    isMovingLeft = true;
   }
-  if (key === "w" || key === "W") {
+  else if (key === "d" || key === "D") {
+    isMovingRight = true;
+  }
+}
+
+function moveShip() {
+  if (isMovingUp) {
     y -= 10;
   }
-  if (key === "s" || key === "S") {
+  if (isMovingDown) {
     y += 10;
+  }
+  if (isMovingLeft) {
+    x -= 10;
+  }
+  if (isMovingRight) {
+    x += 10;
+  }
+}
+
+function keyReleased() {
+  if (key === "w" || key === "W") {
+    isMovingUp = false;
+  }
+  if (key === "s" || key === "S") {
+    isMovingDown = false;
+  }
+  if (key === "a" || key === "A") {
+    isMovingLeft = false;
+  }
+  if (key === "d" || key === "D") {
+    isMovingRight = false;
   }
 }
